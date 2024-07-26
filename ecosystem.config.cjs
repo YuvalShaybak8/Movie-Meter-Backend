598ccef9d1bda60abf2f8097eca9086a34dc762d
yuvals <yuvalsaybak@cs.colman.ac.il>
2024-07-26 13:13:17 +0300

    fix some issue

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,15 +2,15 @@ module.exports = {
   apps: [
     {
       name: "movie-meter-server",
-      script: "./dist/server.js",
+      script: "./dist/src/server.js",
       env_production: {
+        NODE_ENV: "production",
         DATABASE_URL: "mongodb://server:123123123@localhost:21771/web_class",
         TOKEN_SECRET: "76787fda10b163806e5c51ff0c0e3911f4a6e2ef158327db15ee",
         ACCESS_TOKEN_EXPIRATION: "1h",
         REFRESH_TOKEN_EXPIRATION: "14d",
-        HTTPS_PORT: "4000",
-        HTTP_PORT: "80",
-        NODE_ENV: "production",
+        HTTPS_PORT: 443,
+        HTTP_PORT: 80,
         GOOGLE_CLIENT_ID:
           "403571040301-2ka5e09b8upflfk8qmpfebo4sg08gde0.apps.googleusercontent.com",
       },
